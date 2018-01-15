20e79dfe924b93cde07de53b2aef064ea42171b1
taromaru38 <taromaru0308@gmail.com>
2018-01-16 07:22:52 +0900

    問題4-16

---
+++ src/day4/Prob416.java
@@ -0,0 +1,13 @@
+package day4;
+
+public class Prob416 {
+	public static void main(String[] args) {
+
+		for (int i = 1;i < 10;i++) {
+			for (int j = 1;j < 10;j++) {
+				System.out.print(j + "*" + i + "=" + i * j + "　");
+			}
+			System.out.println();
+		}
+	}
+}
