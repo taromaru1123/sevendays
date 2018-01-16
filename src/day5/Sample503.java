998402635083c7b7040df86caf9d3148e39c6c8d
taromaru38 <taromaru0308@gmail.com>
2018-01-16 10:05:52 +0900

    Sample503

---
+++ src/day5/Sample503.java
@@ -0,0 +1,21 @@
+package day5;
+
+public class Sample503 {
+	public static void main(String[] args) {
+		int n[] = {5,4,3,2,1};
+		String s[] = {"ABC","DEF","GHI"};
+
+		int i;
+
+	for (i = 0; i < n.length;i++) {
+		System.out.print(n[i] + " ");
+	}
+	System.out.println();
+
+	for (i = 0;i < s.length;i++) {
+		System.out.print(s[i] + " ");
+	}
+	System.out.println();
+
+	}
+}
