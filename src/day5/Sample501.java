bda634ff8ecec983ad46e4cc75725eb07efd4ad4
taromaru38 <taromaru0308@gmail.com>
2018-01-16 10:05:14 +0900

    Sample501

---
+++ src/day5/Sample501.java
@@ -0,0 +1,18 @@
+package day5;
+
+public class Sample501 {
+	public static void main(String[] args) {
+		double one,two,three;
+		double sum,avg;
+
+		one = 1.2;
+		two = 3.7;
+		three = 4.1;
+		System.out.println(one + " " + two + " " + three);
+		sum = one + two + three;
+		avg = sum / 3.0;
+		System.out.println("合計値：" + sum);
+		System.out.println("平均値：" + avg);
+
+	}
+}
