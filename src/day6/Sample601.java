2d72e3a3112119a861ef6ce2723bbe59e63fad4b
Taromaru <taromaru@niso1985.local>
2018-01-29 23:14:25 +0900

    Sample601

---
+++ src/day6/Sample601.java
@@ -0,0 +1,23 @@
+package day6;
+
+public class Sample601 {
+	public static void main(String[] args) {
+		//インスタンス生成
+		SampleClass01 s = new SampleClass01();
+		//フィールドnに値を代入する
+		s.n = 100;
+		//フィールドsに値を代入
+		s.s = "Hello";
+		//メソッドadd()を呼び出し
+		int ans = s.add(1,2);
+		//メソッドadd()を呼び出し
+		String str = s.add("World");
+		//呼び出し結果の表示
+		System.out.println("ans =" + ans);
+		//呼び出し結果の表示
+		System.out.println("str =" + str);
+
+		s.showNum();
+
+	}
+}
