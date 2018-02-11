13196689f98556d4509b1bfb7a6b5c0bf15d0667
Taromaru <taromaru@niso1985.local>
2018-02-11 17:27:17 +0900

    Test01

---
+++ src/exday1/Test01.java
@@ -0,0 +1,18 @@
+package exday1;
+
+public class Test01 {
+	public static void main(String[] args) {
+        TestClass01 t1 = new TestClass01();
+        TestClass01 t2 = new TestClass01();
+        //  インスタンスの数を表示
+        System.out.println("インスタンス数："+TestClass01.getTest());
+        //  インスタンスを生成する
+        //s1 = new SampleClassEx01(50);   //  コンストラクタ①を呼び出す
+        //s2 = new SampleClassEx01();     //  コンストラクタ②を呼び出す
+        //  値を取得して表示する
+        System.out.println(t1.getValue());
+        System.out.println(t2.getValue());
+        //  インスタンスの数を表示
+        System.out.println("インスタンス数："+SampleClassEx01.getNumberOfInstance());
+    }
+}
